97ac55e5a439027ac99a4640d91a0b5b432aad7c
Mjxkill <mj.electrolab@outlook.com>
2020-11-09 16:36:29 +0100

    Update arduino.py

--- builder/frameworks/arduino.py
+++ builder/frameworks/arduino.py
@@ -7,7 +7,7 @@ from SCons.Script import DefaultEnvironment
 
 env = DefaultEnvironment()
 
-FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-maixduino")
+FRAMEWORK_DIR = env.PioPlatform().get_package_dir("framework-maixduino-mjxkill")
 assert FRAMEWORK_DIR and isdir(FRAMEWORK_DIR)
 SDK_DIR = join(FRAMEWORK_DIR, "cores", "arduino", "kendryte-standalone-sdk")
 
